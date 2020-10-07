c9f1d39bf5c0bd1bd9448469f97c50bca1b70deb
sharifh530 <sharifh530@gmail.com>
2020-10-07 17:53:48 +0600

    Fetch data from backend and serve to frontend

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../../components/Product/Product';
 import products from '../../products';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <h2 style={{ color: 'whitesmoke' }}>Latest products</h2>
+      <h3 style={{ color: 'whitesmoke', margin: '15px 0' }}>Latest products</h3>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
