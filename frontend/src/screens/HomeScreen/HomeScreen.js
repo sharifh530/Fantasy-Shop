b580c50af1404f69449f1b8de41d75c8f36c85e1
sharifh530 <sharifh530@gmail.com>
2021-10-04 15:56:57 +0600

    usermodel, productmodel, ordermodel implemented for mongoDB

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Product from '../../components/Product/Product';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../../components/Product/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
+      const { data } = await axios.get("/api/products");
 
       setProducts(data);
     };
@@ -17,7 +17,7 @@ const HomeScreen = () => {
 
   return (
     <>
-      <h3 style={{ color: 'whitesmoke', margin: '15px 0' }}>Latest products</h3>
+      <h3 style={{ color: "whitesmoke", margin: "15px 0" }}>Latest products</h3>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
